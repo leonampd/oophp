c81d73171a13afff644daa05912a3cfdaa4192ec
Leonam Pereira Dias <leonam.pd@gmail.com>
2018-09-25 22:21:04 -0300

    aula3: classes abstratas não podem ser instanciadas

--- aula3/exemplos/frota_de_carros/programa.php
+++ aula3/exemplos/frota_de_carros/programa.php
@@ -5,32 +5,22 @@ require 'Onibus.php';
 require 'Bike.php';
 require 'Oficina.php';
 
-$meuVeiculo = new Veiculo('123345', 'preto', 'ETF-2355');
-
-echo $meuVeiculo->getChassi();
-echo $meuVeiculo->getPlaca();
-echo $meuVeiculo->getCor();
-
 $onibus = new Onibus('654321', 'branco', 'XYZ-4545', 'catraca eletronica');
 
 echo PHP_EOL;
 
-echo $onibus->getChassi();
-echo $onibus->getPlaca();
-echo $onibus->getCor();
-
-echo PHP_EOL;
+echo 'Chassi: ' . $onibus->getChassi() . PHP_EOL;
+echo 'Placa: ' . $onibus->getPlaca() . PHP_EOL;
+echo 'Cor: ' . $onibus->getCor() . PHP_EOL;
 
 $onibus->andar();
 
-echo PHP_EOL;
+echo PHP_EOL . '-------------------------------------' . PHP_EOL;
 
 $minhabike = new Bike('1233567', 'vermelha', 'ETF-0998');
-
 echo $minhabike->getPlaca();
 
-echo PHP_EOL;
+echo PHP_EOL . '-------------------------------------' . PHP_EOL;
 
 $oficina = new Oficina($minhabike);
-
 $oficina->arrumar();
